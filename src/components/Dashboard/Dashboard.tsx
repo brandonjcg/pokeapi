0cce708d444556ccb1d6417f47a74dd2df8585b5
Brandon Castillo <brajoel98@gmail.com>
2024-07-23 22:58:19 -0700

    feat(api): :alien: implement PokeAPI

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,15 +1,34 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { PokemonCard } from '../PokemonCard';
 import { CombatList } from '../CombatList';
+import { APIS } from '../../api';
+
+interface Pokemon {
+  name: string;
+  img: string;
+}
 
 export const Dashboard = () => {
   const [selectedPokemon, setSelectedPokemon] = useState<string[]>([]);
+  const [pokemonList, setPokemonList] = useState<
+    { name: string; img: string }[]
+  >([]);
 
-  const pokemonList = Array.from({ length: 151 }, (_, index) => ({
-    id: index + 1,
-    name: `Pokemon ${index + 1}`,
-    img: 'url-to-image',
-  }));
+  useEffect(() => {
+    const fetchPokemon = async () => {
+      const response = await fetch(`${APIS.API_URL_POKEAPI}limit=151&offset=0`);
+      const data = await response.json();
+      const formattedPokemon = data.results.map(
+        (pokemon: Pokemon, index: number) => ({
+          name: pokemon.name,
+          img: `${APIS.API_URL_POKE_IMG}${index + 1}.png`,
+        }),
+      );
+      setPokemonList(formattedPokemon);
+    };
+
+    fetchPokemon();
+  }, []);
 
   const handleAddToCombat = (pokemon: string) => {
     setSelectedPokemon([...selectedPokemon, pokemon]);
@@ -29,7 +48,7 @@ export const Dashboard = () => {
           <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
             {pokemonList.map((pokemon) => (
               <PokemonCard
-                key={pokemon.id}
+                key={pokemon.name}
                 name={pokemon.name}
                 img={pokemon.img}
                 onAddToCombat={() => handleAddToCombat(pokemon.name)}
